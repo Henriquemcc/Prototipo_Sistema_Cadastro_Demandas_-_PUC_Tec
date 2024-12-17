edfcb7fc3fcc3dcf53c3a6df6b82bf87bfafc9c9
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-12-17 15:22:45 -0300

    Adicionando classe Role

---
+++ src/main/kotlin/br/pucminas/puctec/sistema/cadastro/demandas/model/Role.kt
@@ -0,0 +1,16 @@
+package br.pucminas.puctec.sistema.cadastro.demandas.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
+
+@Entity
+data class Role(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @Size(max= 50) @NotNull
+    val nome: String,
+)

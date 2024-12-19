7569a4455d25be09dcdb88501717595983f53df7
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-12-19 16:03:39 -0300

    Adicionando entidade

---
+++ src/main/kotlin/br/pucminas/puctec/sistema/cadastro/demandas/model/Professor.kt
@@ -0,0 +1,18 @@
+package br.pucminas.puctec.sistema.cadastro.demandas.model
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
+
+@Entity
+data class Professor(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+    @ManyToOne
+    val area: Area,
+    @OneToOne
+    var pessoa: Pessoa
+)

290a0436b289dd6c826a5154c18880d49805f3ef
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-12-19 16:07:03 -0300

    Adicionando sql para criar tabela Professor

---
+++ src/main/resources/db/migration/V9__create_table_professor.sql
@@ -0,0 +1,8 @@
+create table professor(
+    id bigint not null auto_increment,
+    area_id bigint,
+    pessoa_id bigint,
+    primary key(id),
+    foreign key pessoa_id references pessoa(id),
+    foreign key area_id references area(id)
+);

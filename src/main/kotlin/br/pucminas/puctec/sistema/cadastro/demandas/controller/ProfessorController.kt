7110a14f89497c373f0e3f84655b8f0dca02a555
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-12-20 18:34:34 -0300

    Adicionando ProfessorDtoService no ProfessorController

--- src/main/kotlin/br/pucminas/puctec/sistema/cadastro/demandas/controller/ProfessorController.kt
+++ src/main/kotlin/br/pucminas/puctec/sistema/cadastro/demandas/controller/ProfessorController.kt
@@ -1,31 +1,33 @@
 package br.pucminas.puctec.sistema.cadastro.demandas.controller
 
-import br.pucminas.puctec.sistema.cadastro.demandas.model.Professor
-import br.pucminas.puctec.sistema.cadastro.demandas.service.ProfessorService
+import br.pucminas.puctec.sistema.cadastro.demandas.dto.AtualizarProfessorForm
+import br.pucminas.puctec.sistema.cadastro.demandas.dto.NovoProfessorForm
+import br.pucminas.puctec.sistema.cadastro.demandas.dto.ProfessorView
+import br.pucminas.puctec.sistema.cadastro.demandas.service.ProfessorDtoService
 import jakarta.transaction.Transactional
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/professores")
 class ProfessorController(
-    private val professorService: ProfessorService
+    private val professorDtoService: ProfessorDtoService
 ) {
 
     @GetMapping
-    fun listar(): List<Professor> = professorService.listar()
+    fun listar(): List<ProfessorView> = professorDtoService.listar()
 
     @GetMapping("/{idProfessor}")
-    fun buscarPorId(@PathVariable idProfessor: Long): Professor = professorService.buscarPorId(idProfessor)
+    fun buscarPorId(@PathVariable idProfessor: Long): ProfessorView = professorDtoService.buscarPorId(idProfessor)
 
     @PostMapping
     @Transactional
-    fun cadastrar(@RequestBody professor: Professor): Professor = professorService.cadastrar(professor)
+    fun cadastrar(@RequestBody professor: NovoProfessorForm): ProfessorView = professorDtoService.cadastrar(professor)
 
     @PutMapping("/{idProfessor}")
     @Transactional
-    fun atualizar(@RequestBody professor: Professor, @PathVariable idProfessor: Long): Professor = professorService.atualizar(professor, idProfessor)
+    fun atualizar(@RequestBody professor: AtualizarProfessorForm, @PathVariable idProfessor: Long): ProfessorView = professorDtoService.atualizar(professor, idProfessor)
 
     @DeleteMapping("/{idProfessor}")
     @Transactional
-    fun deletar(@PathVariable idProfessor: Long) = professorService.deletar(idProfessor)
+    fun deletar(@PathVariable idProfessor: Long) = professorDtoService.deletar(idProfessor)
 }

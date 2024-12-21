3513a98a5b3e1030491a1b814398b3b8c8579bab
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-12-21 15:37:54 -0300

    Corrigindo erros de sintaxe SQL

--- src/main/resources/db/migration/V13__alter_table_aluno_add_area_and_professor_orientador.sql
+++ src/main/resources/db/migration/V13__alter_table_aluno_add_area_and_professor_orientador.sql
@@ -1,5 +1,4 @@
 alter table aluno
-    add column area_id bigint,
-    add column professor_orientador_id bigint,
-    add constraint fk_area foreign key (area_id) references area (id),
-    add constraint fk_professor_orientador foreign key (professor_orientador_id) references professor_orientador (id);
+    add column area_id bigint;
+alter table aluno
+    add constraint fk_area foreign key (area_id) references area (id);
